7a796691f26533e27fbeff5feae401013f5f99d8
GavinFoo <fuxiaopang@gmail.com>
2015-11-16 05:49:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# 在CentOS上安装最新的FFMPEG
+## 包含 X264 | AAC | MP3 模块
+## 已经在 CentOS 7 中测试
+
+### 安装
+
+```
+wget https://raw.github.com/GavinFoo/install_ffmpeg/master/install_ffmpeg.sh
+chmod +x install_ffmpeg.sh
+./install_ffmpeg.sh
+```
+
---
+++ install_ffmpeg.sh
@@ -0,0 +1,55 @@
+#http://trac.ffmpeg.org/wiki/CompilationGuide/Centos
+
+yum install autoconf automake cmake freetype-devel gcc gcc-c++ git libtool make mercurial nasm pkgconfig zlib-devel
+
+mkdir ~/ffmpeg_sources
+
+cd ~/ffmpeg_sources
+git clone --depth 1 git://github.com/yasm/yasm.git
+cd yasm
+autoreconf -fiv
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+make distclean
+
+
+cd ~/ffmpeg_sources
+git clone --depth 1 git://git.videolan.org/x264
+cd x264
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+make
+make install
+make distclean
+
+
+cd ~/ffmpeg_sources
+git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
+cd fdk-aac
+autoreconf -fiv
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+make distclean
+
+
+cd ~/ffmpeg_sources
+curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+tar xzvf lame-3.99.5.tar.gz
+cd lame-3.99.5
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
+make
+make install
+make distclean
+
+
+cd ~/ffmpeg_sources
+git clone --depth 1 git://source.ffmpeg.org/ffmpeg
+cd ffmpeg
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --pkg-config-flags="--static" --enable-gpl --enable-nonfree --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libx264
+make
+make install
+make distclean
+hash -r
+
+echo "\nDone\n"
