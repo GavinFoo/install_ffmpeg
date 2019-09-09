b95d808b29f17174eee4b73d94cceb57dca26563
Gavin Foo <fuxiaopang@gmail.com>
2019-09-09 19:36:53 +0800

    Update install_ffmpeg.sh

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,38 +1,39 @@
 #http://trac.ffmpeg.org/wiki/CompilationGuide/Centos
 
-yum install autoconf automake cmake freetype-devel gcc gcc-c++ git libtool make mercurial nasm pkgconfig zlib-devel -y
-yum remove nasm && hash -r
-
+yum install autoconf automake bzip2 bzip2-devel cmake freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig zlib-devel
 mkdir ~/ffmpeg_sources
 
 cd ~/ffmpeg_sources
-curl -O -L http://www.nasm.us/pub/nasm/releasebuilds/2.13.02/nasm-2.13.02.tar.bz2
-tar xjvf nasm-2.13.02.tar.bz2
-cd nasm-2.13.02
+curl -O -L https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.bz2
+tar xjvf nasm-2.14.02.tar.bz2
+cd nasm-2.14.02
 ./autogen.sh
 ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
 make
 make install
-make distclean
 
 cd ~/ffmpeg_sources
-curl -O -L http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+curl -O -L https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
 tar xzvf yasm-1.3.0.tar.gz
 cd yasm-1.3.0
 ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
 make
 make install
-make distclean
 
 
 cd ~/ffmpeg_sources
-git clone --depth 1 git://git.videolan.org/x264
+git clone --depth 1 https://code.videolan.org/videolan/x264.git
 cd x264
 PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
 make
 make install
-make distclean
 
+cd ~/ffmpeg_sources
+hg clone https://bitbucket.org/multicoreware/x265
+cd ~/ffmpeg_sources/x265/build/linux
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
+make
+make install
 
 cd ~/ffmpeg_sources
 git clone --depth 1 https://github.com/mstorsjo/fdk-aac
@@ -42,15 +43,21 @@ autoreconf -fiv
 make
 make install
 
-
 cd ~/ffmpeg_sources
-curl -O -L http://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz
+curl -O -L https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz
 tar xzvf lame-3.100.tar.gz
 cd lame-3.100
 ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
 make
 make install
 
+cd ~/ffmpeg_sources
+curl -O -L https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz
+tar xzvf opus-1.3.1.tar.gz
+cd opus-1.3.1
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
 
 cd ~/ffmpeg_sources
 git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
@@ -58,6 +65,15 @@ cd libvpx
 ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm
 make
 make install
+
+# Install libaom from source.
+mkdir -p ~/ffmpeg_sources/libaom && \
+  cd ~/ffmpeg_sources/libaom && \
+  git clone https://aomedia.googlesource.com/aom && \
+  cmake ./aom && \
+  make && \
+  sudo make install
+
 
 cd ~/ffmpeg_sources
 curl -O -L https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
@@ -75,11 +91,14 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-libfdk_aac \
   --enable-libfreetype \
   --enable-libmp3lame \
+  --enable-libopus \
   --enable-libvpx \
   --enable-libx264 \
+  --enable-libx265 \
+  --enable-libaom \
   --enable-nonfree
 make
 make install
-hash -r
+hash -d ffmpeg
 
 echo "\nDone\n"
